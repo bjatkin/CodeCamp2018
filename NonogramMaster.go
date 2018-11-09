a75b0697145dcf2cd50ff15bd70f2351acd7e692
Coby Johnson <i2c2-caj@users.noreply.github.com>
2018-11-09 13:55:42 -0700

    Assign tasks to the workers

--- NonogramMaster.go
+++ NonogramMaster.go
@@ -4,6 +4,7 @@ type Master struct {
 	Board *Board
 	MovesIn <-chan Move
 	MovesOut chan<- Move
+	Tasks chan Method
 	Workers []Worker
 }
 
@@ -12,16 +13,29 @@ func NewMaster(board *Board) (m *Master) {
 	m.Board = board
 	m.MovesIn = make(<-chan Move, WorkerCount)
 	m.MovesOut = make(chan<- Move, WorkerCount)
-	m.Workers = make([]Worker, 0)
+	m.Workers = make([]Worker, WorkerCount)
 	for i := 0; i < WorkerCount; i++ {
-		m.newWorker()
+		m.newWorker(i)
+	}
+	m.Tasks = make(chan Method, MethodCount)
+	for i := 0; i < int(MethodCount); i++ {
+		m.Tasks <- Method(i)
 	}
 	return
 }
 
-func (m Master) newWorker() {
-	m.Workers = append(m.Workers, Worker {
+func (m Master) newWorker(id int) {
+	m.Workers[id] = Worker {
+		Id: id,
+		Board: m.Board,
 		MovesIn: make(<-chan Move, WorkerCount),
 		MovesOut: make(chan<- Move, WorkerCount),
-	})
+	}
+}
+
+func (m Master) Solve() {
+	for i:= 0; i < WorkerCount; i++ {
+		m.Workers[i].Task = <-m.Tasks
+		m.Workers[i].Solve()
+	}
 }
--- NonogramWorker.go
+++ NonogramWorker.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Worker struct {
+	Id int
 	Board *Board
 	MovesIn <-chan Move
 	MovesOut chan<- Move
+	Task Method
+}
+
+func (w Worker) Solve() {
+	fmt.Printf("Worker[%d] working on '%d'\n", w.Id, w.Task)
 }
