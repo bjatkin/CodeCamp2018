4f7f779a19a663a45496e5156177814a8d0812ae
Coby Johnson <i2c2-caj@users.noreply.github.com>
2018-11-09 15:23:46 -0700

    Fixed issue resolving max number of workers that could be used

--- NonogramMaster.go
+++ NonogramMaster.go
@@ -1,25 +1,27 @@
 package main
 
+import "math/rand"
+
 type Master struct {
 	Board *Board
-	MovesIn <-chan Move
-	MovesOut chan<- Move
-	Tasks chan Method
+	MovesIn chan Move
+	MovesOut chan Move
+	Tasks []Method
 	Workers []Worker
 }
 
 func NewMaster(board *Board) (m *Master) {
 	m = &Master{}
 	m.Board = board
-	m.MovesIn = make(<-chan Move, WorkerCount)
-	m.MovesOut = make(chan<- Move, WorkerCount)
+	m.MovesIn = make(chan Move, WorkerCount)
+	m.MovesOut = make(chan Move, WorkerCount)
 	m.Workers = make([]Worker, WorkerCount)
 	for i := 0; i < WorkerCount; i++ {
 		m.newWorker(i)
 	}
-	m.Tasks = make(chan Method, MethodCount)
+	m.Tasks = make([]Method, MethodCount)
 	for i := 0; i < int(MethodCount); i++ {
-		m.Tasks <- Method(i)
+		m.Tasks[i] = Method(i)
 	}
 	return
 }
@@ -29,13 +31,20 @@ func (m Master) newWorker(id int) {
 		Id: id,
 		Board: m.Board,
 		MovesIn: make(<-chan Move, WorkerCount),
-		MovesOut: make(chan<- Move, WorkerCount),
+		MovesOut: m.MovesIn,
 	}
 }
 
 func (m Master) Solve() {
 	for i:= 0; i < WorkerCount; i++ {
-		m.Workers[i].Task = <-m.Tasks
+		m.ShuffleMethods()
+		m.Workers[i].Tasks = m.Tasks
 		m.Workers[i].Solve()
 	}
+}
+
+func (m Master) ShuffleMethods() {
+	rand.Shuffle(len(m.Tasks), func(i, j int) {
+		m.Tasks[i], m.Tasks[j] = m.Tasks[j], m.Tasks[i]
+	})
 }
