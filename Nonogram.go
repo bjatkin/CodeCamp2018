88ca27684a354a636192fecbc248d30c4cf2ace4
Brandon Atkinson <bj.atkinson92@gmail.com>
2018-11-09 10:24:18 -0700

    basic boilerplate/test for wasm

--- Nonogram.go
+++ Nonogram.go
@@ -1,24 +1,25 @@
 package CodeCamp2018
 
+import "github.com/dennwc/dom/svg"
+
 type Mark int
 
 const (
-	Fill Mark = 0
+	Fill  Mark = 0
 	Cross Mark = 1
 )
 
 type Move struct {
-	X int
-	Y int
+	X    int
+	Y    int
 	Mark Mark
 }
 
 type Board struct {
+	SVG         *svg.Rect
 	ColumnCount int
-	RowCount int
-	BoardMarks [][]Mark
+	RowCount    int
+	BoardMarks  [][]Mark
 	ColumnHints [][]int
-	RowHints [][]int
+	RowHints    [][]int
 }
-
-
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package CodeCamp2018
 
+import (
+	"fmt"
 
+	"github.com/dennwc/dom/svg"
+)
+
+const (
+	Pcolor = "#0416300"
+	Scolor = "#5a8bbc"
+	Black  = "#000020"
+	White  = "#f4f4ff"
+)
 
 func main() {
+	fmt.Println("test")
 
+	w, h := 300.0, 300.0
+
+	root := svg.NewFullscreen()
+	center := root.NewG()
+	center.Translate(w/2, h/2)
+
+	mainBoard := Board{
+		SVG: center.NewRect(10, 10),
+	}
+	mainBoard.SVG.SetAttribute("fill", Pcolor)
 }
