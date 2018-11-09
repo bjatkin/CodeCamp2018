97980a30515196e9afb5e7d40150e73877a75691
Coby Johnson <i2c2-caj@users.noreply.github.com>
2018-11-09 13:55:25 -0700

    Added in a method struct
    used to track what the workers are working on

--- Nonogram.go
+++ Nonogram.go
@@ -13,3 +13,17 @@ type Move struct {
 	Y    int
 	Mark Mark
 }
+
+type Method int
+
+const (
+	Boxes Method = 0
+	Spaces Method = 1
+	Forcing Method = 2
+	Glue Method = 3
+	Joining Method = 4
+	Splitting Method = 5
+	Punctuating Method = 6
+	Mercury Method = 7
+	MethodCount Method = 8
+)
